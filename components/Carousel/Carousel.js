1f43719f557553227ff6820af45d72e0b29cb636
Arron M <marshall.arron@gmail.com>
2019-04-12 10:39:23 -0700

    Add previous slide animation

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,31 +4,68 @@ class Carousel {
         this.left = this.carousel.querySelector('.left-button');
         this.right = this.carousel.querySelector('.right-button');
 
-        this.images = Array.from(this.carousel.querySelectorAll('img')); // DEBUG: making this an array here may cause issues if node methods aren't exposed
+        this.images = Array.from(this.carousel.querySelectorAll('img'));
         this.numberOfImages = this.images.length;
         this.currentIndex = 1;
+        this.currentSlide = this.images[this.currentIndex - 1]
+        this.nextSlide = this.images[this.getSlideIndex(this.currentIndex + 1) - 1];
+        this.previousSlide = this.images[this.getSlideIndex(this.currentIndex - 1) -1];
 
-        // TODO: Add nextImg and previousImg here? Refactor?
+        this.left.addEventListener('click', () => this.previousSlideTrigger());
+        this.right.addEventListener('click', () => this.nextSlideTrigger());
 
-        this.left.addEventListener('click', () => this.previousSlide());
-        this.right.addEventListener('click', () => this.nextSlide());
+        // Immediately display our initial carousel image
+        this.currentSlide.style.display = 'block';
 
-        this.carousel.querySelector(`img[data-slide="${this.currentIndex}"]`).style.display = 'block'; // TODO: Probably better to have a class method here
+        // Set up our previous and next images for animation
+        this.nextSlide.classList.add('img--next');
+        this.previousSlide.classList.add('img--previous');
+        this.carousel.insertBefore(this.previousSlide, this.currentSlide);
     }
 
-    previousSlide() {
-        // TODO: (FOR ANIMATION) Hide current +1, Show current -1, add classes img--next and img--previous
+    previousSlideTrigger() {
+        this.currentIndex = this.getSlideIndex(this.currentIndex - 1);
 
-        this.carousel.querySelector(`img[data-slide="${this.currentIndex}"]`).style.display = 'none';
-        this.currentIndex = (this.currentIndex + 1) > this.numberOfImages ? 1 : this.currentIndex + 1;
-        this.carousel.querySelector(`img[data-slide="${this.currentIndex}"]`).style.display = 'block';
+        this.nextSlide.classList.remove('img--next');
+        this.currentSlide.classList.add('img--next');
+        this.nextSlide = this.currentSlide;
 
+        this.previousSlide.classList.remove('img--previous');
+        this.currentSlide = this.previousSlide;
+        this.currentSlide.style.display = 'block';
+
+        this.previousSlide = this.images[this.getSlideIndex(this.currentIndex - 1) - 1];
+        this.previousSlide.classList.add('img--previous');
+        this.carousel.insertBefore(this.previousSlide, this.currentSlide);
     }
 
-    nextSlide() {
-        this.carousel.querySelector(`img[data-slide="${this.currentIndex}"]`).style.display = 'none';
-        this.currentIndex = (this.currentIndex - 1) < 1 ? this.numberOfImages : this.currentIndex - 1;
-        this.carousel.querySelector(`img[data-slide="${this.currentIndex}"]`).style.display = 'block';
+    nextSlideTrigger() {
+        // this.currentIndex = this.getSlideIndex(this.currentIndex + 1);
+
+        // this.previousSlide.classList.remove('img--previous');
+        // this.currentSlide.classList.add('img--previous');
+        // this.carousel.appendChild(this.previousSlide);
+        // this.previousSlide = this.currentSlide;
+
+        // this.nextSlide.classList.remove('img-next');
+
+        // this.nextSlide = this.images[this.getSlideIndex(this.currentIndex + 1) - 1];
+        // this.nextSlide.classList.add('img--next');
+
+        // this.nextSlide.classList.remove('img--next');
+        // this.currentSlide = this.nextSlide;
+        // this.currentSlide.style.display = 'block';
+    }
+
+    /**
+     * Handles math for changing our slide index, returns a number within our slide range
+     * @param {number} index - Expects a slide index number.
+     * @return {number}
+     */
+    getSlideIndex(index) {
+        if (index > this.numberOfImages) return 1;
+        if (index < 1) return this.numberOfImages;
+        return index;
     }
 }
 
